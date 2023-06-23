4bd256b23643dd28e6cce81f6fb8e71e3915eb2d
Sphericalkat <amolele@gmail.com>
2023-06-23 09:46:26 +0530

    fix(treewide): remove unnecessary code
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- lib/diffutils/structs/edit_op.dart
+++ lib/diffutils/structs/edit_op.dart
@@ -4,9 +4,4 @@ class EditOp {
   EditType? type;
   int? spos; // source block pos
   int? dpos; // destination block pos
-
-  @override
-  String toString() {
-    return '$type($spos,$dpos)';
-  }
 }
